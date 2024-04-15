1ac3220899fc726babf869004a28eb4c17ad22a8
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2024-04-15 19:58:56 +0200

    login signup nav

--- app/src/main/java/com/example/androidview/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/example/androidview/ui/signup/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidview.ui.signup
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidview.databinding.ActivitySignupBinding
@@ -21,6 +22,9 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         binding.signupButton.setOnClickListener {
             val username = binding.newUsername.text.toString()
             val password = binding.newPassword.text.toString()
@@ -33,5 +37,18 @@ class SignupActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG).show()
             }
         }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // This ID represents the Home or Up button; in case of your activity, it's Up
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
