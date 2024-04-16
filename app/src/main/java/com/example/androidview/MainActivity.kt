aed8db33e489662b49326143aaa8a1d3ece0fc6e
ana-zrnic <ana.zrnic@student.pmf.unibl.org>
2024-04-16 19:17:06 +0200

    logout implemented

--- app/src/main/java/com/example/androidview/MainActivity.kt
+++ app/src/main/java/com/example/androidview/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.androidview
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.androidview.databinding.ActivityMainBinding
+import com.example.androidview.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        //val toolbar: Toolbar = findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -50,7 +56,24 @@ class MainActivity : AppCompatActivity() {
                 onBackPressed()
                 return true
             }
+            R.id.action_logout -> {
+                // Add your logout logic here
+                performLogout()
+                return true
+            }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun performLogout() {
+        // For example, start the login activity and finish the current one
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        return true
+    }
+
 }
